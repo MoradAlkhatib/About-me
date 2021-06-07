878f869e3ce49db44033b0b4fedbb45327e3d8bf
moradalkhatib <muradalkhateb@icloud.com>
2021-06-08 00:27:18 +0300

    last

---
+++ js/app.js
@@ -0,0 +1,87 @@
+let userName=prompt("What The Name Of Programmer This Page ..Answer Murad");
+
+var user=userName.substr();
+if(user==="murad"){
+    alert("You Have A correct");
+}
+else{
+    alert("unfortunately You Have Uncorrect Answer");
+}
+
+let work=prompt("Is The Programmer For This Page Love Work ..Answer Yes Or No or y or n");
+
+var works=work.substr();
+if(works=="yes"||"y"){
+    alert("You Have A correct Answer");
+}
+else if(works=="no"||"n"){
+    alert("unfortunately You Have Uncorrect Answer");
+}
+else {
+    alert("Please Answer Yes , No , y , n");
+}
+// end work
+let work=prompt("Is The Programmer Work As A Electric Assistant ..Answer Yes Or No or y or n");
+
+var works=work.substr();
+if(works=="yes"||"y"){
+    alert("You Have A correct Answer");
+}
+else if(works=="no"||"n"){
+    alert("unfortunately You Have Uncorrect Answer");
+}
+else {
+    alert("Please Answer Yes , No , y , n");
+}
+// 
+let worker=prompt("Is The Programmer  Work As A selling manager ..Answer Yes Or No or y or n");
+
+var workers=work.substr();
+if(workers=="n"||"no"){
+    alert("You Have A correct Answer");
+}
+else if(workers=="yes"||"y"){
+    alert("unfortunately You Have Uncorrect Answer");
+}
+else {
+    alert("Please Answer Yes , No , y , n");
+}
+//
+let study=prompt("Is The Programmer Have A Degree Of Comuter Information System ..Answer Yes Or No or y or n");
+
+var studys=work.substr();
+if(studys=="yes"||"no"){
+    alert("You Have A correct Answer");
+}
+else if(studys=="y"||"n"){
+    alert("unfortunately You Have Uncorrect Answer");
+}
+else {
+    alert("Please Answer Yes , No , y , n");
+}
+//
+let degree=prompt("Is The Programmer Have Accounting Degree ..Answer Yes Or No or y or n");
+
+var degrees=work.substr();
+if(degrees=="n"||"no"){
+    alert("You Have A correct Answer");
+}
+else if(works=="yes"||"y"){
+    alert("unfortunately You Have Uncorrect Answer");
+}
+else {
+    alert("Please Answer Yes , No , y , n");
+}
+//
+let speek=prompt("Is The Programmer Speek Spanish ..Answer Yes Or No or y or n");
+
+var speeks=work.substr();
+if(speeks=="n"||"no"){
+    alert("You Have A correct Answer");
+}
+else if(speeks=="yes"||"n"){
+    alert("unfortunately You Have Uncorrect Answer");
+}
+else {
+    alert("Please Answer Yes , No , y , n");
+}
