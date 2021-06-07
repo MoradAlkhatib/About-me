64ea2d96f0c87d4d681f312f929ff7ffd5f4cbd7
moradalkhatib <muradalkhateb@icloud.com>
2021-06-08 00:58:02 +0300

    last

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 let userName=prompt("What The Name Of Programmer This Page ..Answer Murad");
 
-var user=userName.substr();
+var user=userName.toLowerCase();
 if(user=="murad"){
     alert("You Have A correct");
 }
@@ -11,7 +11,7 @@ else{
 
 let work=prompt("Is The Programmer For This Page Love Work ..Answer Yes Or No or y or n");
 
-var works=work.substr();
+var works=work.toLowerCase();
 if(works=="yes"||works=="y"){
     alert("You Have A correct Answer");
 }
@@ -24,7 +24,7 @@ else {
 // end work
 let wor=prompt("Is The Programmer Work As A Electric Assistant ..Answer Yes Or No or y or n");
 
-var wors=wor.substr();
+var wors=wor.toLowerCase();
 if(wors=="yes"||wors=="y"){
     alert("You Have A correct Answer");
 }
@@ -37,7 +37,7 @@ else {
 // 
 let worker=prompt("Is The Programmer  Work As A selling manager ..Answer Yes Or No or y or n");
 
-var workers=worker.substr();
+var workers=worker.toLowerCase();
 if(workers=="n"||workers=="no"){
     alert("You Have A correct Answer");
 }
@@ -50,7 +50,7 @@ else {
 //
 let study=prompt("Is The Programmer Have A Degree Of Comuter Information System ..Answer Yes Or No or y or n");
 
-var studys=study.substr();
+var studys=study.toLowerCase();
 if(studys=="yes"||studys=="no"){
     alert("You Have A correct Answer");
 }
@@ -63,7 +63,7 @@ else {
 //
 let degree=prompt("Is The Programmer Have Accounting Degree ..Answer Yes Or No or y or n");
 
-var degrees=degree.substr();
+var degrees=degree.toLowerCase();
 if(degrees=="n"||degrees=="no"){
     alert("You Have A correct Answer");
 }
@@ -76,7 +76,7 @@ else {
 //
 let speek=prompt("Is The Programmer Speek Spanish ..Answer Yes Or No or y or n");
 
-var speeks=speek.substr();
+var speeks=speek.toLowerCase();
 if(speeks=="n"||speeks=="no"){
     alert("You Have A correct Answer");
 }
