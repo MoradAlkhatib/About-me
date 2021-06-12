49281fd41c9312fb6a78837b238519438d13f3eb
moradalkhatib <muradalkhateb@icloud.com>
2021-06-12 05:17:01 +0300

    l

--- js/app.js
+++ js/app.js
@@ -1,122 +1,117 @@
 "use strict";
-var scour=0;
-let usern=prompt("Please Enter You'r Name");
-alert('Hello  '+usern+'  You are Welcom');
-alert(usern+' We Have A Seven Question For U About Me Lest Start With These Question');
+ var score=0;
+// let usern=prompt("Please Enter You'r Name");
+// alert('Hello  '+usern+'  You are Welcom');
+// alert(usern+' We Have A Seven Question For You ......');
+ 
 
-let userName=prompt("What The Name Of Programmer This Page ..Answer Murad");
-
-var user=userName.toLowerCase();
-if(user=="murad"){
-    alert("You Have A correct");
-    scour++;
-}
-else{
-    alert("unfortunately You Have Uncorrect Answer");
-}
-
-let work=prompt("Is The Programmer For This Page Love Work ..Answer Yes Or No or y or n");
-
-var works=work.toLowerCase();
-if(works=="yes"||works=="y"){
-    alert("You Have A correct Answer");
-    scour++;
-}
-else if(works=="no"||works=="n"){
-    alert("unfortunately You Have Uncorrect Answer");
-}
-else {
-    alert("Please Answer Yes , No , y , n");
-}
-// end work
-let wor=prompt("Is The Programmer Work As A Electric Assistant ..Answer Yes Or No or y or n");
-
-var wors=wor.toLowerCase();
-if(wors=="yes"||wors=="y"){
-    alert("You Have A correct Answer");
-    scour++;
-}
-else if(wors=="no"||wors=="n"){
-    alert("unfortunately You Have Uncorrect Answer");
-}
-else {
-    alert("Please Answer Yes , No , y , n");
-}
-// 
-let worker=prompt("Is The Programmer  Work As A selling manager ..Answer Yes Or No or y or n");
-
-var workers=worker.toLowerCase();
-if(workers=="n"||workers=="no"){
-    alert("You Have A correct Answer");
-    scour++;
-}
-else if(workers=="yes"||workers=="y"){
-    alert("unfortunately You Have Uncorrect Answer");
-}
-else {
-    alert("Please Answer Yes , No , y , n");
-}
-//
-let study=prompt("Is The Programmer Have A Degree Of Comuter Information System ..Answer Yes Or No or y or n");
-
-var studys=study.toLowerCase();
-if(studys=="yes"||studys=="y"){
-    alert("You Have A correct Answer");
-    scour++;
-}
-else if(studys=="no"||studys=="n"){
-    alert("unfortunately You Have Uncorrect Answer");
-}
-else {
-    alert("Please Answer Yes , No , y , n");
-}
-//
-let g=prompt("whats My old ? Only Number Is Allowed");
-var guess=parseInt(g);
-
-for(var i =0;i<3;i++)
-{
-   if(guess===24)
-   {alert("Thats Correct Ansewr ");
-   scour++;
-   break;
-}
-else if(guess>24){
-    alert("thats high Hot..! ");
-    guess=prompt("whats My old ?")
-}
-else if(guess<24)
-{
-    alert("Thats Low Cold..!");
-    guess=prompt("whats My old ?");
-}
-
-}
-if(guess!=24)
-alert("Tha Correct Answer Is 24");
+function yesOrnoQuestion(question,correctAns,uncorrescAns) {
+    let yourAnswer;
+    do {
+      alert(' Please Answer This Question With Yes , No , Y , N');
+      yourAnswer=prompt(question).toLowerCase();
+    } while (yourAnswer!=='yes' && yourAnswer!=='y' && yourAnswer!=='no' && yourAnswer!=='n');
+    if (yourAnswer==='yes' || yourAnswer==='y') {
+      alert(correctAns);
+      score++;
+    }else  {
+      alert(uncorrescAns);
+    }
+    
+  }
 
 
-let c =prompt('Try To Know One Of I Loved Car').toLowerCase();
-var cars = ["saab", "volvo", "bmw"];
-for(var s=0;s<5;s++){
 
- if(c==cars[0]||c==cars[1]||c==cars[2])
- {
-     alert('You Have A correct Answer');
-     scour++;
-     break;
- }
- else{
-    c=prompt('Try To Know One Of I Loved Car').toLowerCase(); 
- }
+function guessAge(){
+   
+    let g;
+    
+    var guess;
+    var s=0;
+    
+    
+    for(var i =0;i<4;i++)
+    {
+        g=prompt("whats My old ? Only Number Is Allowed");
+        if (isNaN(g)){
+            alert("You Can Use Tust A Numbers")
+             s=i;
+             i=s;
+           }
+           else{
+               i=s;
+               s++;
+            guess =parseInt(g);
+       if(guess===24)
+       {alert("Thats Correct Ansewr ");
+       score++;
+       break;
+    }
+    else if(guess>24){
+        alert("thats high Hot..! ");
+        guess=prompt("whats My old ?")
+    }
+    else if(guess<24)
+    {
+        alert("Thats Low Cold..!");
+        guess=prompt("whats My old ?");
+    }
+}
+    }
+    if(guess!=24)
+    alert("Tha Correct Answer Is 24");
+    
+
 
 }
 
-if(c!=cars[0]&&c!=cars[1]&&c!=cars[2]){
-    alert("unfortunately You Cant Have A Correct Answer..But  My Cars Is : "+cars);
+
+
+function guessTopTen(){
+    let c =prompt('Try To Know One Of I Loved Car').toLowerCase();
+    var cars = ["saab", "volvo", "bmw"];
+    for(var s=0;s<5;s++){
+    
+     if(c==cars[0]||c==cars[1]||c==cars[2])
+     {
+         alert('You Have A correct Answer');
+         score++;
+         break;
+     }
+     else{
+        c=prompt('Try To Know One Of I Loved Car').toLowerCase(); 
+     }
+    
+    }
+    
+    if(c!=cars[0]&&c!=cars[1]&&c!=cars[2]){
+        alert("unfortunately You Cant Have A Correct Answer..But  My Cars Is : "+cars);
+    }
+
 }
 
-alert("You'r Scour Is "+scour+"/7");
+// yesOrnoQuestion('Is The Programmer For This Page Love Work','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is The Programmer Work As A Electric Assistant','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is A Maui, Hawaii A One Of Top Ten To Visiting','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is The Programmer Have A Degree Of Comuter Information System','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is The Programmer Talk English Language','You Have A Correct Answer','UnCorrect Answer');
+  guessAge();
+//  guessTopTen();
+
+// alert("You'r Scour Is "+score+"/7");
 
 
-alert('Good Bye '+usern);
+// alert('Good Bye '+usern);
+
+// let input;
+// while (input = prompt("Input number: ")) {
+//   if (isNaN(input)) {
+//     alert("Invalid input.");
+    
+//   } else {
+//     alert("valid input.");
+//     break;
+//   }
+// }
+
+
--- js/lab Pair.js
+++ js/lab Pair.js
@@ -1,24 +1,51 @@
-// 'use strict';
+'use strict';
 
-// alert('Pleas Answer With Yes ,No , y ,n');
-// function ques(quest,correctAns,uncorrescAns) {
-//   let myprompt;
+alert('Pleas Answer With Yes ,No , y ,n');
+function yesOrnoQuestion(question,correctAns,uncorrescAns) {
+  let yourAnswer;
+  do {
+    yourAnswer=prompt(question).toLowerCase();
+  } while (yourAnswer!=='yes' && yourAnswer!=='y' && yourAnswer!=='no' && yourAnswer!=='n');
+  if (yourAnswer==='yes' || yourAnswer==='y') {
+    alert(correctAns);
+    score++;
+  }else {
+    alert(uncorrescAns);
+  }
+}
+
+
+
+ques('Is The Programmer For This Page Love Work', 'yes' , 'no');
+ques('Is The Programmer Work As A Electric Assistant Is The Programmer Work As A Electric Assistant', 'yes' , 'no');
+ques('Is The Programmer  Work As A selling manager' , 'yes' , 'no');
+ques(' is The Name Of Programmer Of this Page Is Murad', 'yes' , 'no');
+ques('Is The Programmer Have A Degree Of Comuter Information System' , 'yes' , 'no');
+
+
+// function yesOrnoQuestion(question,correctAnswer,uncorrectAnswer){
+//     let yourAnswer;
+  
 //   do {
-//     myprompt=prompt(quest).toLowerCase();
-//   } while (myprompt!=='yes' && myprompt!=='y' && myprompt!=='no' && myprompt!=='n');
-//   if (myprompt==='yes' || myprompt==='y') {
-//     alert(correctAns);
-//   }else {
-//     alert(uncorrescAns);
+
+//       yourAnswer =prompt(question).toLowerCase();
+
+//   } while (yourAnswer!=='yes' && yourAnswer!=='y' && yourAnswer=='no' && yourAnswer!=='n');
+  
+//   if(yourAnswer=='yes' || yourAnswer=='y')
+//   {
+//       alert(correctAnswer);
+//       score++;
+//   }
+//   else
+//   {
+//       alert(uncorrectAnswer);
+
 //   }
 // }
 
-
-
-// ques('Is The Programmer For This Page Love Work', 'yes' , 'no');
-// ques('Is The Programmer Work As A Electric Assistant Is The Programmer Work As A Electric Assistant', 'yes' , 'no');
-// ques('Is The Programmer  Work As A selling manager' , 'yes' , 'no');
-// ques(' is The Name Of Programmer Of this Page Is Murad', 'yes' , 'no');
-// ques('Is The Programmer Have A Degree Of Comuter Information System' , 'yes' , 'no');
-
-
+// yesOrnoQuestion('Is The Programmer For This Page Love Work','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is The Programmer Work As A Electric Assistant','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is A Maui, Hawaii A One Of Top Ten To Visiting','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is The Programmer Have A Degree Of Comuter Information System','You Have A Correct Answer','UnCorrect Answer');
+// yesOrnoQuestion('Is The Programmer Talk English Language','You Have A Correct Answer','UnCorrect Answer');
